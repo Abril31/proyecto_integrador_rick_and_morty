f277ec48b166af5a8649bf5331155cfb43027f7f
Abril31 <brivaldi0416@gmail.com>
2023-09-08 00:19:17 -0400

    Avance back-end

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,24 +1,16 @@
 const http = require('http');
-const characters = require('./utils/data');
+const getCharById = require('./controllers/')
 const PORT = 3001;
 
  http.createServer((req, res)=>{
     const { url } = req;
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    //localhost:3001/rickandmorty/character/${id}ya tiene una barra por default
+    //localhost:3001/rickandmorty/character/${id} ya tiene una barra por default
 
     if( url.includes("rickandmorty/character")){
         //extraer el id por medio de split y pop
-        let urlID = url.split('/').pop()
-        let findID = characters.find(character =>{
-            character.id = Number(urlID)
-        })
-        res.writeHead(200, {
-            "Content-Type": "application/jason"
-        })
-       
-        return res.end(JSON.stringify(findID));
+        let urlID = url.split('/').pop();
+        getCharById(res, urlID)
     }
-
 }).listen(PORT);
