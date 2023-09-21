d312d12597846d88c603e057951dca3fd5ccea03
Abril31 <brivaldi0416@gmail.com>
2023-09-21 18:04:58 -0400

    Avance

--- Client/src/DB_connection.js
+++ Client/src/DB_connection.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const UserModel = require("../../Server/src/models/User");
+const FavoriteModel = require("../../Server/src/models/Favorite");
 
 // EJERCICIO 03
 // A la instancia de Sequelize le falta la URL de conexión. ¡Agrégala!
@@ -14,17 +16,19 @@ const sequelize = new Sequelize(
 
 // EJERCICIO 05
 // Debajo de este comentario puedes ejecutar la función de los modelos.
-
+UserModel(sequelize);
 //
-
+FavoriteModel(sequelize);
 //
 
 // Ejercicio 06
 // ¡Relaciona tus modelos aquí abajo!
-// const { User, Favorite } = sequelize.models;
+const { User, Favorite } = sequelize.models;
+User.belongsToMany(Favorite, { through: "user_favorite" });
+Favorite.belongsToMany(User, { through: "user_favorite" });
 
 module.exports = {
-  // User,
-  // Favorite,
+  User,
+  Favorite,
   conn: sequelize,
 };
