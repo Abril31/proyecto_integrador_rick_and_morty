0bc15f863d7fe339c5a0920cc42f2393957a6c14
Abril31 <brivaldi0416@gmail.com>
2024-06-04 20:56:15 -0400

    Icons added

--- front/components/Detail/Detail.jsx
+++ front/components/Detail/Detail.jsx
@@ -2,6 +2,9 @@ import "./Detail.css";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import male from "/icons/male.svg";
+import female from "/icons/female.svg";
+import unknown from "/icons/question.svg";
 
 const Detail = () => {
   let { id } = useParams();
@@ -12,6 +15,7 @@ const Detail = () => {
       ({ data }) => {
         if (data.name) {
           setCharacter(data);
+          console.log(data);
         } else {
           window.alert("No hay personajes con ese ID");
         }
@@ -23,22 +27,32 @@ const Detail = () => {
   return (
     <section className="card-cont">
       <div className="info-char">
-        <div className="column">
-          <h3 className="text">NAME: {character?.name}</h3>
+        <div className="text-cont">
           <h3 className="text">STATUS: {character?.status}</h3>
           <h3 className="text">SPECIES: {character?.species}</h3>
-        </div>
-        <div className="column">
-          <h3 className="text">GENDER: {character?.gender}</h3>
+
           <h3 className="text">ORIGIN: {character?.origin}</h3>
         </div>
-        {/* <h3>LOCATION: {character?.origin?.name}</h3> */}
       </div>
-      <img
-        className="img-detail"
-        src={character?.image}
-        alt={character?.name}
-      />
+      <div>
+        <img
+          className="img-detail"
+          src={character?.image}
+          alt={character?.name}
+        />
+        <div className="text-under">
+          <h3 className="text-name">{character?.name}</h3>
+          <h3 className="text-name">
+            {character?.gender === "Male" ? (
+              <img src={male} alt="Male" />
+            ) : character?.gender === "Female" ? (
+              <img src={female} alt="Female" />
+            ) : (
+              <img src={unknown} alt="Unknown" />
+            )}
+          </h3>
+        </div>
+      </div>
     </section>
   );
 };
